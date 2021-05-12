c2b4db72e89007b41a6dd59f7a363ba6f593a4bc
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-12 18:21:02 +0000

    converts string to int

--- _strtol.c
+++ _strtol.c
@@ -11,18 +11,20 @@ int _strtol(char *str, unsigned int line)
 int base = 10;
 int num;
 char *endptr;
+
+errno = 0;
 num = strtol(str, &endptr, base);
 
 if (endptr == str)
 {
 /* None digits were found */
-printf("L%u: usage: push integer\n", line);
+printf("L%d: usage: push integer\n", line);
 exit(EXIT_FAILURE);
 }
 if (str[0] != '\0')
 if (!isdigit(str[0]) && *endptr != '\0')
 {
-printf("L%u: usage: push integer\n", line);
+printf("L%d: usage: push integer\n", line);
 exit(EXIT_FAILURE);
 }
 return (num);
