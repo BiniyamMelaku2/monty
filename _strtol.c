5e31d125be616c6b285a9632dc784574de44f7a0
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-12 18:01:24 +0000

    converts string to int

--- _strtol.c
+++ _strtol.c
@@ -1,8 +1,14 @@
 #include "monty.h"
 
+/**
+ * _strtol - converts string to int
+ * @str: string operand input argument
+ * @line: the current file line number calling instruction
+ * Return: exitcodes upon failure or converted number
+ */
 int _strtol(char *str, unsigned int line)
 {
-int base = 10, c;
+int base = 10;
 int num;
 char *endptr;
 num = strtol(str, &endptr, base);
@@ -13,13 +19,11 @@ if (endptr == str)
 printf("L%u: usage: push integer\n", line);
 exit(EXIT_FAILURE);
 }
-for (c = 0; str[c]; c++)
-{
-/*if (!isdigit(str[c]))
+if (str[0] != '\0')
+if (!isdigit(str[0]) && *endptr != '\0')
 {
 printf("L%u: usage: push integer\n", line);
 exit(EXIT_FAILURE);
-}*/
 }
 return (num);
 }
