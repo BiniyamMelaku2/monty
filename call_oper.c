ec8fa9b2b8a727d6d18ac1cb31af0dc83333d603
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-12 18:00:48 +0000

    executes instructions on stack

--- call_oper.c
+++ call_oper.c
@@ -6,6 +6,7 @@
  * @oper: string copy of stack instruction
  * @line: the current file line number calling instruction
  */
+
 void call_oper(stack_t **stack, char *oper, unsigned int line)
 {
 int c;
@@ -13,6 +14,18 @@ instruction_t all_opers[] = {
 {"push", instruct_push},
 {"pall", instruct_pall},
 {"pint", instruct_pint},
+{"pop", instruct_pop},
+{"swap", instruct_swap},
+{"add", instruct_add},
+{"nop", instruct_nop},
+{"sub", instruct_sub},
+{"div", instruct_div},
+{"mul", instruct_mul},
+{"mod", instruct_mod},
+{"pchar", instruct_pchar},
+{"pstr", instruct_pstr},
+{"rotl", instruct_rotl},
+{"rotr", instruct_rotr},
 {NULL, NULL}
 };
 
