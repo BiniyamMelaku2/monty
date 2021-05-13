41abc929c8db7c57d224d17a3b7054c0cbcf7c53
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-13 08:52:28 +0000

    the monty main program entry point

--- monty.c
+++ monty.c
@@ -1,33 +1,59 @@
 #include "monty.h"
 
-/* globally accessble stack struct type variable */
-gl_stack mystack;
-
-/**
- * stack_init - initialize stack top to head and head to null
- * @head: new created stack
- */
-
-void stack_init(stack_t **head)
-{
-*head = NULL;
-mystack.top = head;
-}
+/* global struct to hold flag for queue and stack length */
+var_t var;
 
 /**
  * free_stack - frees malloced in stack nodes memory
+ * @arg: double pointer to the stack
+ * @status: exit status
  */
-void free_stack(void)
+void free_stack(int status, void *arg)
 {
-stack_t *tmp = NULL, *head;
-head = *mystack.top;
-while (head != NULL)
+stack_t **stack;
+stack_t *tmp;
+(void) status;
+stack = (stack_t **)arg;
+if (*stack)
 {
-tmp = head->next;
-free(head);
-head = tmp;
+(*stack)->prev->next = NULL;
+(*stack)->prev = NULL;
 }
+while (*stack != NULL)
+{
+tmp = (*stack)->next;
+free(*stack);
+*stack = tmp;
 }
+var.len_stack = 0;
+}
+
+/**
+ * free_lineptr - free line pointer returned by getline
+ * @status: exit status
+ * @arg: pointer to line
+ */
+void free_lineptr(int status, void *arg)
+{
+char **lineptr = arg;
+(void) status;
+if (*lineptr != NULL)
+free(*lineptr);
+}
+
+/**
+ * myfile_close - close file stream
+ * @status: status passed to exit
+ * @arg: pointer to file stream
+ */
+void myfile_close(int status, void *arg)
+{
+FILE *myfile;
+(void) status;
+myfile = (FILE *) arg;
+fclose(myfile);
+}
+
 
 /**
  * main - monty bytecode interpreter
@@ -36,15 +62,40 @@ head = tmp;
  * Return: exitcodes for failure or 0 Success
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-stack_t *head;
-stack_init(&head);
+stack_t *stack = NULL;
+unsigned int line = 0;
+FILE *myfile = NULL;
+char *lineptr = NULL, *oper = NULL;
+size_t llen = 0;
+
+var.len_queue = 0;
+var.len_stack = 0;
 if (argc != 2)
 {
-fprintf(stdout,"USAGE: monty file\n");
+dprintf(STDOUT_FILENO, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-execute_file(argv[1], &head);
+myfile = fopen(argv[1], "r");
+if (myfile == NULL)
+{
+dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+
+on_exit(free_lineptr, &lineptr);
+on_exit(free_stack, &stack);
+on_exit(myfile_close, myfile);
+
+while (getline(&lineptr, &llen, myfile) != -1)
+{
+line++;
+oper = strtok(lineptr, "\n\t\r ");
+if (oper != NULL && oper[0] != '#')
+{
+call_oper(&stack, oper, line);
+}
+}
 exit(EXIT_SUCCESS);
 }
