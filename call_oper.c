3a5c62a230ef2ef7ccf5baf41ed6d8759c191e86
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-05-13 08:55:54 +0000

    executes instructions of stack

--- call_oper.c
+++ call_oper.c
@@ -26,10 +26,12 @@ instruction_t all_opers[] = {
 {"pstr", instruct_pstr},
 {"rotl", instruct_rotl},
 {"rotr", instruct_rotr},
+{"stack", set_stack},
+{"queue", set_queue},
 {NULL, NULL}
 };
 
-for (c = 0; all_opers[c].opcode; c++)
+for (c = 0; all_opers[c].opcode != NULL; c++)
 {
 if (strcmp(oper, all_opers[c].opcode) == 0)
 {
@@ -37,10 +39,6 @@ all_opers[c].f(stack, line);
 return;
 }
 }
-if (strlen(oper) != 0 && oper[0] != '#')
-{
-printf("L%u: unknown instruction %s\n", line, oper);
+dprintf(STDOUT_FILENO, "L%u: unknown instruction %s\n", line, oper);
 exit(EXIT_FAILURE);
 }
-}
-
